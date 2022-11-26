a6e2c1856e69c6f9dd87ad223bb21adb50bfd5d9
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2022-11-26 14:07:05 -0600

    catagorize projects into language

--- .config/zsh/xdg.sh
+++ .config/zsh/xdg.sh
@@ -6,6 +6,6 @@ export XDG_STATE_HOME=$HOME/.local/state
 export XDG_CACHE_HOME=$HOME/.cache
 
 export LESSHISTFILE=/dev/null
-export XAUTHORITY=/tmp/Xauthority
+export XAUTHORITY=~/.cache/xauthority
 export CARGO_HOME=~/.local/share/cargo
 export RUSTUP_HOME=~/.local/share/rustup
