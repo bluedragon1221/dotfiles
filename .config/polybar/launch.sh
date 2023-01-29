346c8374be9f57a54a1b5fcb2b06ddc1ac3bebe3
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-01-29 21:36:37 +0000

    no polybar

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-# wait for polybar to shutdown
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-(sleep 2; polybar --config=~/.config/polybar/polybar.config mainbar) &
